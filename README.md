98883adab419cf1d5bdf9255c74dbefd99bc515d
Ishan Pranav <ishan.pranav@outlook.com>
2025-01-27 18:36:01 -0500

    1/22

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+# Classes
 
+This is a demonstration of classes implemented in Python for the NYU CSCI 479
+Data Management and Analysis course.
 
+## `BitList`
+
+This is a data structure that represents a bit vector.
+
+## Books
+
+This is a simple Jupyter Notebook that illustrates file I/O and string data
+manipulation in Python.
+
+## License
+
+This repository is licensed with the [MIT](LICENSE.txt) license.
--- bits.py
+++ bits.py
@@ -1,4 +1,17 @@
-# Part 1 goes here!
-class BitList:
-    pass
+# bits.py
+# Copyright (c) 2025 Ishan Pranav
+# Licensed under the MIT license.
 
+class DecodeError(Exception):
+    pass
+    
+class ChunkError(Exception):
+    pass
+    
+class BitList:
+    def __init__(self, value):
+        try:
+            self.value = int(value, 2)
+        except ValueError:
+            raise ValueError(
+                "Format is invalid; does not consist of only 0 and 1")
